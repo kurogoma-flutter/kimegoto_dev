0402ce9c4dd1dcba7b7e4637ee529931d280d0d6
Kurogoma939 <kan.sss.939@gmail.com>
2022-08-16 12:28:01 +0900

    ログ出力実装

--- lib/utils/logger.dart
+++ lib/utils/logger.dart
@@ -1,28 +1,38 @@
-// ignore: depend_on_referenced_packages
 import 'package:logger/logger.dart';
+import 'package:intl/intl.dart';
 
 final logger = Logger(
-  printer: PrettyPrinter(
-    methodCount: 1, // 表示されるコールスタックの数
-    errorMethodCount: 5, // 表示されるスタックトレースのコールスタックの数
-    lineLength: 120, // 出力するログ1行の幅
-    colors: true, // メッセージに色をつける
-    printEmojis: true, // 絵文字を出力
-    printTime: true, // タイムスタンプを出力
-  ),
-  output: ConsoleOutput(),
+  printer: CustomLogPrinter(),
+  output: CustomLogOutput(),
 );
 
-var loggerNoStack = Logger(
-  printer: PrettyPrinter(methodCount: 0),
-);
+class CustomLogPrinter extends LogPrinter {
+  @override
+  List<String> log(LogEvent event) {
+    final message = event.message;
 
-class ConsoleOutput extends LogOutput {
+    String msg;
+    if (message is Function()) {
+      msg = message().toString();
+    } else if (message is String) {
+      msg = message;
+    } else {
+      msg = message.toString();
+    }
+    return [
+      '[${event.level.name.toUpperCase()}] '
+          '${DateFormat('HH:mm:ss.SSS').format(DateTime.now())}: '
+          '$msg'
+    ];
+  }
+}
+
+class CustomLogOutput extends ConsoleOutput {
   @override
   void output(OutputEvent event) {
-    for (var line in event.lines) {
-      // ignore: avoid_print
-      print(line);
+    super.output(event);
+    if (event.level.index >= Level.error.index) {
+      throw AssertionError('StackTraceを参照してください。');
     }
   }
 }
